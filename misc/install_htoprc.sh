3188a7c72bd72523ae9c5fcd43e0ce4dba8bf431
Jonathan G Rennison <j.g.rennison@gmail.com>
2014-06-06 19:10:38 +0100

    Make htoprc an optional copy-based install. Update htoprc.

---
+++ misc/install_htoprc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright © Jonathan G. Rennison 2014 <j.g.rennison@gmail.com>
+# License: New BSD License, see BSD-LICENSE.txt
+
+# This installs the associated copy of htoprc in ~/.config/htop/htoprc
+# This is not done by default a some machines have a customised htoprc
+
+THISDIR="$(readlink -f $(dirname "$0"))"
+
+HTOPRC="$HOME/.config/htop/htoprc"
+BCKUP="$HOME/.config/htop/htoprc-`date -u "+%Y%m%dT%H%M%SZ"`"
+SRC="$THISDIR/htoprc"
+
+if [ -e "$HTOPRC" -o -L "$HTOPRC" ]; then
+	echo "Backing up old htoprc to $BCKUP"
+	mv "$HTOPRC" "$BCKUP"
+fi
+
+echo "Installing new htoprc"
+cp "$THISDIR/htoprc" "$HOME/.config/htop/htoprc"
