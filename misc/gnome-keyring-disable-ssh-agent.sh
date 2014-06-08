8a5408ae3abf727e8289b785f4f1c313c6584254
Jonathan G Rennison <j.g.rennison@gmail.com>
2014-06-08 14:01:00 +0100

    Add headers and sanity checks to gnome keyring and engb locale scripts.

--- misc/gnome-keyring-disable-ssh-agent.sh
+++ misc/gnome-keyring-disable-ssh-agent.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# Copyright © Jonathan G. Rennison 2014 <j.g.rennison@gmail.com>
+# License: New BSD License, see BSD-LICENSE.txt
+
+# This script replaces gnome-keyring-daemon with a wrapper which invokes the original
+# with the ssh agent disabled.
+# This is because it is otherwise overly cumbersome to disable its ssh-agent on some platforms.
+# This is based on this post http://askubuntu.com/a/412794 by JanKanis.
+
+if [ -e "/usr/bin/gnome-keyring-daemon-wrapped" ]; then
+	echo "Wrapper seems to be already installed, aborting" &>2
+	exit 1
+fi
+
+if [ '!' -w "/usr/bin/" ]; then
+	echo "Can't write to /usr/bin/, maybe try as root? Aborting" &>2
+	exit 1
+fi
+
 mv /usr/bin/gnome-keyring-daemon /usr/bin/gnome-keyring-daemon-wrapped
 echo '#!/bin/sh' > /usr/bin/gnome-keyring-daemon
 echo 'exec /usr/bin/gnome-keyring-daemon-wrapped --components=pkcs11,secrets,gpg "$@"' >> /usr/bin/gnome-keyring-daemon
--- misc/modify_engb_locale_dates.sh
+++ misc/modify_engb_locale_dates.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+# Copyright © Jonathan G. Rennison 2014 <j.g.rennison@gmail.com>
+# License: New BSD License, see BSD-LICENSE.txt
+
+# This script installs a replacement modified en_GB locale which uses ISO 8601 dates
+
+THISDIR="$(readlink -f $(dirname "$0"))"
+
+if [ -e "/usr/share/i18n/locales/en_GB.orig" ]; then
+	echo "Seems to be already installed, aborting" &>2
+	exit 1
+fi
+
+if [ '!' -w "/usr/share/i18n/locales/" ]; then
+	echo "Can't write to /usr/share/i18n/locales/, maybe try as root? Aborting" &>2
+	exit 1
+fi
+
 mv /usr/share/i18n/locales/en_GB /usr/share/i18n/locales/en_GB.orig
-cp en_GB@custom /usr/share/i18n/locales/en_GB
+cp "$THISDIR/en_GB@custom" /usr/share/i18n/locales/en_GB
 pushd /usr/share/i18n/locales/ &> /dev/null
 localedef -f UTF-8 -i en_GB en_GB.UTF-8
 popd &> /dev/null
