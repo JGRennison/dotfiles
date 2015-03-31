3d5198889daed0f5f0a49ea16a70fcd3fed8e87d
Jonathan G Rennison <j.g.rennison@gmail.com>
2015-03-31 01:21:52 +0100

    Add yet more hacks to prevent Gnome keyring clobbering the ssh-agent.

--- misc/gnome-keyring-disable-ssh-agent.sh
+++ misc/gnome-keyring-disable-ssh-agent.sh
@@ -5,11 +5,17 @@
 
 # This script replaces gnome-keyring-daemon with a wrapper which invokes the original
 # with the ssh agent disabled.
-# This is because it is otherwise overly cumbersome to disable its ssh-agent on some platforms.
+# This is because it is not otherwise practical to disable its ssh-agent.
 # This is based on this post http://askubuntu.com/a/412794 by JanKanis.
+# and this post http://askubuntu.com/a/584061 by Matthew Caron.
 
 dpkg-divert --divert /usr/bin/gnome-keyring-daemon-wrapped --rename /usr/bin/gnome-keyring-daemon \
 	|| dpkg-divert --divert /usr/bin/gnome-keyring-daemon-wrapped /usr/bin/gnome-keyring-daemon
+
+if [ '!' -f "$HOME/.config/autostart/gnome-keyring-ssh.desktop" ]; then
+	echo -e "[Desktop Entry]\nX-GNOME-Autostart-enabled=false" | ${SUDO_USER:+ sudo -u $SUDO_USER} tee "$HOME/.config/autostart/gnome-keyring-ssh.desktop" > /dev/null
+	echo "Writing to $HOME/.config/autostart/gnome-keyring-ssh.desktop" >&2
+fi
 
 if [ "`readlink -f "/usr/bin/gnome-keyring-daemon"`" == "/usr/bin/gnome-keyring-daemon-wrapper" ]; then
 	echo "Wrapper seems to be already installed, aborting" >&2
